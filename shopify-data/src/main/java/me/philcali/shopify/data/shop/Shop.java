fc350195174574e9c34f1dfbb18657727b0152f3
Philip Cali <philip.cali@gmail.com>
2017-07-08 20:24:06 -0400

    Need a shop object... that's pretty important.

---
+++ shopify-data/src/main/java/me/philcali/shopify/data/shop/Shop.java
@@ -0,0 +1,375 @@
+package me.philcali.shopify.data.shop;
+
+import java.util.Date;
+
+public class Shop {
+    private String address1;
+    private String address2;
+    private String city;
+    private String country;
+    private String countryCode;
+    private String countryName;
+    private Date createdAt;
+    private Date updatedAt;
+    private String customerEmail;
+    private String currency;
+    private String domain;
+    private String email;
+    private String googleAppsDomain;
+    private String googleAppsLoginEnabled;
+    private Long id;
+    private String latitude;
+    private String longitude;
+    private String moneyFormat;
+    private String moneyWithCurrencyFormat;
+    private String weightUnit;
+    private String myshopifyDomain;
+    private String name;
+    private String planName;
+    private boolean hasDiscounts;
+    private boolean hasGiftCards;
+    private String planDisplayName;
+    private String phone;
+    private String primaryLocale;
+    private String province;
+    private String provinceCode;
+    private String shopOwner;
+    private String source;
+    private boolean forceSsl;
+    private String taxShipping;
+    private String taxesIncluded;
+    private String countyTaxes;
+    private String timezone;
+    private String ianaTimezone;
+    private String zip;
+    private boolean hasStorefront;
+    private boolean setupRequired;
+
+    public String getAddress1() {
+        return address1;
+    }
+
+    public String getAddress2() {
+        return address2;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public String getCountryCode() {
+        return countryCode;
+    }
+
+    public String getCountryName() {
+        return countryName;
+    }
+
+    public String getCountyTaxes() {
+        return countyTaxes;
+    }
+
+    public Date getCreatedAt() {
+        return createdAt;
+    }
+
+    public String getCurrency() {
+        return currency;
+    }
+
+    public String getCustomerEmail() {
+        return customerEmail;
+    }
+
+    public String getDomain() {
+        return domain;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getGoogleAppsDomain() {
+        return googleAppsDomain;
+    }
+
+    public String getGoogleAppsLoginEnabled() {
+        return googleAppsLoginEnabled;
+    }
+
+    public String getIanaTimezone() {
+        return ianaTimezone;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getLatitude() {
+        return latitude;
+    }
+
+    public String getLongitude() {
+        return longitude;
+    }
+
+    public String getMoneyFormat() {
+        return moneyFormat;
+    }
+
+    public String getMoneyWithCurrencyFormat() {
+        return moneyWithCurrencyFormat;
+    }
+
+    public String getMyshopifyDomain() {
+        return myshopifyDomain;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public String getPlanDisplayName() {
+        return planDisplayName;
+    }
+
+    public String getPlanName() {
+        return planName;
+    }
+
+    public String getPrimaryLocale() {
+        return primaryLocale;
+    }
+
+    public String getProvince() {
+        return province;
+    }
+
+    public String getProvinceCode() {
+        return provinceCode;
+    }
+
+    public String getShopOwner() {
+        return shopOwner;
+    }
+
+    public String getSource() {
+        return source;
+    }
+
+    public String getTaxesIncluded() {
+        return taxesIncluded;
+    }
+
+    public String getTaxShipping() {
+        return taxShipping;
+    }
+
+    public String getTimezone() {
+        return timezone;
+    }
+
+    public Date getUpdatedAt() {
+        return updatedAt;
+    }
+
+    public String getWeightUnit() {
+        return weightUnit;
+    }
+
+    public String getZip() {
+        return zip;
+    }
+
+    public boolean isForceSsl() {
+        return forceSsl;
+    }
+
+    public boolean isHasDiscounts() {
+        return hasDiscounts;
+    }
+
+    public boolean isHasGiftCards() {
+        return hasGiftCards;
+    }
+
+    public boolean isHasStorefront() {
+        return hasStorefront;
+    }
+
+    public boolean isSetupRequired() {
+        return setupRequired;
+    }
+
+    public void setAddress1(String address1) {
+        this.address1 = address1;
+    }
+
+    public void setAddress2(String address2) {
+        this.address2 = address2;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public void setCountryCode(String countryCode) {
+        this.countryCode = countryCode;
+    }
+
+    public void setCountryName(String countryName) {
+        this.countryName = countryName;
+    }
+
+    public void setCountyTaxes(String countyTaxes) {
+        this.countyTaxes = countyTaxes;
+    }
+
+    public void setCreatedAt(Date createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public void setCurrency(String currency) {
+        this.currency = currency;
+    }
+
+    public void setCustomerEmail(String customerEmail) {
+        this.customerEmail = customerEmail;
+    }
+
+    public void setDomain(String domain) {
+        this.domain = domain;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setForceSsl(boolean forceSsl) {
+        this.forceSsl = forceSsl;
+    }
+
+    public void setGoogleAppsDomain(String googleAppsDomain) {
+        this.googleAppsDomain = googleAppsDomain;
+    }
+
+    public void setGoogleAppsLoginEnabled(String googleAppsLoginEnabled) {
+        this.googleAppsLoginEnabled = googleAppsLoginEnabled;
+    }
+
+    public void setHasDiscounts(boolean hasDiscounts) {
+        this.hasDiscounts = hasDiscounts;
+    }
+
+    public void setHasGiftCards(boolean hasGiftCards) {
+        this.hasGiftCards = hasGiftCards;
+    }
+
+    public void setHasStorefront(boolean hasStorefront) {
+        this.hasStorefront = hasStorefront;
+    }
+
+    public void setIanaTimezone(String ianaTimezone) {
+        this.ianaTimezone = ianaTimezone;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public void setLatitude(String latitude) {
+        this.latitude = latitude;
+    }
+
+    public void setLongitude(String longitude) {
+        this.longitude = longitude;
+    }
+
+    public void setMoneyFormat(String moneyFormat) {
+        this.moneyFormat = moneyFormat;
+    }
+
+    public void setMoneyWithCurrencyFormat(String moneyWithCurrencyFormat) {
+        this.moneyWithCurrencyFormat = moneyWithCurrencyFormat;
+    }
+
+    public void setMyshopifyDomain(String myshopifyDomain) {
+        this.myshopifyDomain = myshopifyDomain;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public void setPlanDisplayName(String planDisplayName) {
+        this.planDisplayName = planDisplayName;
+    }
+
+    public void setPlanName(String planName) {
+        this.planName = planName;
+    }
+
+    public void setPrimaryLocale(String primaryLocale) {
+        this.primaryLocale = primaryLocale;
+    }
+
+    public void setProvince(String province) {
+        this.province = province;
+    }
+
+    public void setProvinceCode(String provinceCode) {
+        this.provinceCode = provinceCode;
+    }
+
+    public void setSetupRequired(boolean setupRequired) {
+        this.setupRequired = setupRequired;
+    }
+
+    public void setShopOwner(String shopOwner) {
+        this.shopOwner = shopOwner;
+    }
+
+    public void setSource(String source) {
+        this.source = source;
+    }
+
+    public void setTaxesIncluded(String taxesIncluded) {
+        this.taxesIncluded = taxesIncluded;
+    }
+
+    public void setTaxShipping(String taxShipping) {
+        this.taxShipping = taxShipping;
+    }
+
+    public void setTimezone(String timezone) {
+        this.timezone = timezone;
+    }
+
+    public void setUpdatedAt(Date updatedAt) {
+        this.updatedAt = updatedAt;
+    }
+
+    public void setWeightUnit(String weightUnit) {
+        this.weightUnit = weightUnit;
+    }
+
+    public void setZip(String zip) {
+        this.zip = zip;
+    }
+}
